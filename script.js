cc6914f18fc8db7a83e25ac700e4dd784e1bfbbe
thevisualriot <rydzkowska@gmail.com>
2023-11-29 21:15:37 +0000

    save to local storage and update array of profiles

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /* --------------------------------------------------- VARIABLES ---------------------------------------------------- */
+var inputEl = document.querySelector("#b-day");
+var SaveButtonEl = document.querySelector("#submit");
+var pictureCardEl = document.querySelector("#picture"); // TO BE REPLACED WITH PICTURE HOLDER
 
-
-var birthday = "1994-04-24"; // TO BE REPLACED WITH INPUT
+var birthday; // TO BE REPLACED WITH INPUT
 var birthdayDate = new Date(birthday);
 
 // NASA API RELATED VARIABLES
@@ -9,11 +11,31 @@ var nasaAPIKey = "yO4gV7LKJVWKPZKFc7GlvBh0f5Ig8XZN2KOgjgRp";
 var startDateNasa = new Date(1995, 6, 1);
 var today = new Date();
 
-var pictureCard = document.querySelector("#picture"); // TO BE REPLACED WITH PICTURE HOLDER
+var profiles = [];
+var profile = {
+    name: "",
+    birthday: "",
+}
+
+
+updateProfiles();
+
+SaveButtonEl.addEventListener("click", function(e){
+    e.preventDefault();
+    var bDay = inputEl.value;
+    var bMonth = inputEl.value;
+    var bYear = inputEl.value;
+    birthday = bYear + "-" + bMonth + "-" + bDay;
+    console.log(birthday);
+})
 
 
 
-if(birthdayDate > startDateNasa){
+
+
+
+
+if(birthdayDate > startDateNasa){ 
     fetchNASAPicture(birthday);
 } else {
     var randomDate = new Date(startDateNasa.getTime() + Math.random() * (today.getTime() - startDateNasa.getTime()));
@@ -26,6 +48,31 @@ if(birthdayDate > startDateNasa){
 
 /* -----------------------------------------------------FUNCTIONS ---------------------------------------------------- */
 
+// SAVE TO LOCAL STORAGE
+
+function saveToLocalStorage(profile){
+    localStorage.setItem("profiles", JSON.stringify(profiles));
+}
+
+
+// UPDATE LOCAL STORAGE & DISPLAYED PROFILES
+
+function updateProfiles(){
+    var savedProfiles;
+    if(localStorage){
+        savedProfiles = JSON.parse(localStorage.getItem("profiles"));
+    } else {
+        if(savedProfiles){
+            profiles = savedProfiles;
+        } else {
+            profiles = [];
+        }
+    }
+}
+
+
+// FETCH NASA PICTURE OF THE DAY AND DISPLAY
+
 function fetchNASAPicture(date){
     var nasaQueryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasaAPIKey + "&date="+ date;
     
@@ -35,7 +82,6 @@ function fetchNASAPicture(date){
     })
     .then(function(data){
         var nasaPictureURL = data.url
-        pictureCard.setAttribute("src", nasaPictureURL);    
-    
+        pictureCardEl.setAttribute("src", nasaPictureURL);  
 })
 }
