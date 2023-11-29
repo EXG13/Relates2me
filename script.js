1b1303e91e5cc2f667a4619b59c2331656e814bd
thevisualriot <rydzkowska@gmail.com>
2023-11-29 20:52:04 +0000

    add random date for people older than from 1995

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+/* --------------------------------------------------- VARIABLES ---------------------------------------------------- */
+
+
+var birthday = "1994-04-24"; // TO BE REPLACED WITH INPUT
+var birthdayDate = new Date(birthday);
+
+// NASA API RELATED VARIABLES
 var nasaAPIKey = "yO4gV7LKJVWKPZKFc7GlvBh0f5Ig8XZN2KOgjgRp";
-var birthday = "1996-04-24";
-console.log(birthday);
-var nasaQueryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasaAPIKey + "&date="+ birthday;
-var pictureCard = document.querySelector("#picture");
+var startDateNasa = new Date(1995, 6, 1);
+var today = new Date();
+
+var pictureCard = document.querySelector("#picture"); // TO BE REPLACED WITH PICTURE HOLDER
 
 
-fetch(nasaQueryURL)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    var startDateNasa = new Date("1995-06-16");
-    var birthdayDate = new Date(birthday);
-    var nasaPictureURL = data.url
-    if(birthdayDate > startDateNasa){
-        pictureCard.setAttribute("src", nasaPictureURL);
-        console.log(data);
-    } else{
-        console.log("You too old to get a pic")
-        // console.log(data);
-    }
+
+if(birthdayDate > startDateNasa){
+    fetchNASAPicture(birthday);
+} else {
+    var randomDate = new Date(startDateNasa.getTime() + Math.random() * (today.getTime() - startDateNasa.getTime()));
+    var randomDateFormatted = dayjs(randomDate).format("YYYY-MM-DD");
+    console.log(randomDateFormatted);
+    fetchNASAPicture(randomDateFormatted);
+}
+
+
+
+/* -----------------------------------------------------FUNCTIONS ---------------------------------------------------- */
+
+function fetchNASAPicture(date){
+    var nasaQueryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasaAPIKey + "&date="+ date;
     
+    fetch(nasaQueryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        var nasaPictureURL = data.url
+        pictureCard.setAttribute("src", nasaPictureURL);    
     
 })
+}
