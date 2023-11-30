8e2310818b1cbd2f2246a9a265aa4b4c8c962884
thevisualriot <rydzkowska@gmail.com>
2023-11-30 21:20:08 +0000

    removed unnecessary code to clean

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var inputDayEl = document.querySelector("#day");
 var inputMonthEl = document.querySelector("#month"); 
 var inputYearEl = document.querySelector("#year");
-// var inputNameEl = document.querySelector("#name"); // !! TO UPDATE
+// var inputNameEl = document.querySelector("#name"); // !! TO UPDATE & ENABLE WHEN READY
 var SaveBtnEl = document.querySelector("button"); // UPDATE
-var pictureCardEl = document.querySelector("#picture"); // TO BE REPLACED WITH PICTURE HOLDER
+// var pictureCardEl = document.querySelector("#picture"); // TO UPDATE WHEN READY
 
 var profiles = [];
 var profile = {
@@ -23,83 +23,6 @@ var today = new Date();
 
 
 updateProfiles();
-
-// 'NEXT' BUTTON THAT SAVES THE NAME TO LOCAL STORAGE
-// nextBtn.addEventListener("click", function(e){
-//     inputtedName = inputNameEl.value;
-//     localStorage.setItem("tempName", inputtedName); // it stores the name temporary to link it to birthday on the second button
-// })
-
-
-// 'SAVE' BUTTON THAT SAVES NAME & BIRTHDAY IN LOCAL STORAGE 
-// SaveBtnEl.addEventListener("click", function(e){
-//     console.log("working");
-//     updateProfiles();
-
-//     var bDay = inputDayEl.value;
-//     var bMonth = inputMonthEl.value;
-//     var bYear = inputYearEl.value;
-//     birthday = bYear + "-" + bMonth + "-" + bDay;
-
-//     if(birthdayDate > startDateNasa){ // If birthday is in the range of NASA pic of the day (from 1995)
-//         fetchNASAPicture(birthday);
-//     } else {    // otherwise create a random date and choose a random pic
-//         var randomDate = new Date(startDateNasa.getTime() + Math.random() * (today.getTime() - startDateNasa.getTime()));
-//         var randomDateFormatted = dayjs(randomDate).format("YYYY-MM-DD");
-//         fetchNASAPicture(randomDateFormatted);
-//     }
-
-//     // Create a new profile object and store it in the localStorage
-//     var newProfile = Object.create(profile);
-//     var newProfileName = localStorage.getItem("tempName");
-//     newProfile.name = newProfileName;
-//     newProfile.birthday = birthday;
-//     saveToLocalStorage(newProfile);
-//     localStorage.removeItem("tempName"); // remove temporarily store name, to make a space for a new one
-
-// });
-
-
-
-/*------------------------------------------------------------ TEST ------------------------------------------------- */
-
-
-function submitForm(){
-
-    updateProfiles();
-
-    var name = ""; // UPDATE WITH: inputNameEl.value; - when ready
-
-    var bDay = inputDayEl.value;
-    var bMonth = inputMonthEl.value;
-    var bYear = inputYearEl.value;
-    birthday = bYear + "-" + bMonth + "-" + bDay;
-
-    if(birthdayDate > startDateNasa){ // If birthday is in the range of NASA pic of the day (from 1995)
-        fetchNASAPicture(birthday);
-    } else {    // otherwise create a random date and choose a random pic
-        var randomDate = new Date(startDateNasa.getTime() + Math.random() * (today.getTime() - startDateNasa.getTime()));
-        var randomDateFormatted = dayjs(randomDate).format("YYYY-MM-DD");
-        fetchNASAPicture(randomDateFormatted);
-    }
-
-    // Create a new profile object and store it in the localStorage
-    if (name && birthday){
-        createProfile(name, birthday);
-    } else if (!name){
-        name = "Anonymous";
-        createProfile(name, birthday);
-        console.log(name);
-    } else if (!birthday){
-        birthday = "2000-01-01";
-        createProfile(name, birthday);
-    }
-}
-
-
-/*------------------------------------------------------------ END TEST ------------------------------------------------- */
-
-
 
 
 
@@ -128,6 +51,39 @@ function updateProfiles(){
         }
     }
 
+
+function submitForm(){
+
+    updateProfiles();
+    
+    var name = ""; // UPDATE WITH: inputNameEl.value; - when ready
+    
+    var bDay = inputDayEl.value;
+    var bMonth = inputMonthEl.value;
+    var bYear = inputYearEl.value;
+    birthday = bYear + "-" + bMonth + "-" + bDay;
+    
+    if(birthdayDate > startDateNasa){ // If birthday is in the range of NASA pic of the day (from 1995)
+        fetchNASAPicture(birthday);
+    } else {    // otherwise create a random date and choose a random pic
+        var randomDate = new Date(startDateNasa.getTime() + Math.random() * (today.getTime() - startDateNasa.getTime()));
+        var randomDateFormatted = dayjs(randomDate).format("YYYY-MM-DD");
+        fetchNASAPicture(randomDateFormatted);
+    }
+    
+    // Create a new profile object and store it in the localStorage
+    if (name && birthday){
+        createProfile(name, birthday);
+    } else if (!name){
+        name = "Anonymous";
+        createProfile(name, birthday);
+        console.log(name);
+    } else if (!birthday){
+        birthday = "2000-01-01";
+        createProfile(name, birthday);
+    }
+}
+
 function createProfile(name, birthday){
     var newProfile = Object.create(profile);
     var newProfileName = name;
@@ -149,6 +105,6 @@ function fetchNASAPicture(date){
     })
     .then(function(data){
         var nasaPictureURL = data.url
-        pictureCardEl.setAttribute("src", nasaPictureURL);  
+        // pictureCardEl.setAttribute("src", nasaPictureURL);  // ENABLE WHEN READY
 })
 }
